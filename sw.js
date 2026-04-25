ffa752860c78eef1ea69dab05bd2f5d7273f34b4
Vdskillers <vdskillers@hotmail.com>
2026-04-25 19:22:38 +0200

    Update CACHE_VERSION to ami-v4.5

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    ✅ Préfixe cache isolé (amitest-) pour ne pas entrer en conflit avec la PWA prod
 */
 
-const CACHE_VERSION = 'ami-v4.4';
+const CACHE_VERSION = 'ami-v4.5';
 const CACHE_STATIC  = CACHE_VERSION + '-static';
 const CACHE_TILES   = CACHE_VERSION + '-tiles';
 
